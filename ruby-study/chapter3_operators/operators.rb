d0c07001bfb9f8911f145e087fec0d3aeb9811f9
souson7 <souson7@users.noreply.github.com>
2025-08-25 23:56:43 +0900

    chapter3: 演算子の確認

---
+++ ruby-study/chapter3_operators/operators.rb
@@ -0,0 +1,25 @@
+puts 1+2*3
+puts (1+2)*3
+
+#割り算で小数点以下まで計算したいときは、両方または一方を少数にする
+puts 5/2
+puts 5.0/2.0
+
+#累乗
+puts 2**8
+
+#余剰
+puts 7%3
+
+#比較演算子
+puts 1<2 #true
+puts 1>2 #false
+puts 1<=2 #true
+puts 2>=2 #true
+
+#==（等しければtrue）、!=（等しくなければtrue）は等しいかどうかの判断メソッド
+puts 1==2 #false 
+puts 2==2 #true
+puts 2==1+1 #true
+puts 1 != 2 #true
+puts 2 != 2 #false
