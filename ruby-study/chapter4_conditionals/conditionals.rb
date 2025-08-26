6dd4ccd14c3bac157e163fc1be54438041d05d20
souson7 <souson7@users.noreply.github.com>
2025-08-26 23:46:54 +0900

    chapter4: 条件分岐

---
+++ ruby-study/chapter4_conditionals/conditionals.rb
@@ -0,0 +1,104 @@
+#if-条件を満たすとき
+wallet = 500
+if wallet >= 300
+    puts "コンビニでアイスを買っていこう！"
+end
+
+season = "夏"
+if season == "夏"
+    puts "かき氷たべたい"
+    puts "麦茶のみたい"
+end
+
+#if-条件を満たさないとき
+wallet = 100
+if wallet >= 300
+    puts "コンビニでお菓子を買っていこう！"
+end
+#何も表示されない
+
+#unlessと!
+x = 200
+if x != 100
+    puts "100ではありません"
+end
+
+season = "春"
+if season != "夏"
+    puts "あんまんたべたい"
+end
+
+x = 200
+unless x == 100
+    puts "100ではありません"
+end
+
+#そうでなければ-else節
+wallet = 100
+if wallet >= 300
+    puts "コンビニでアイスを買っていこう！"
+else
+    puts "家に帰ろう" #条件を満たさなかった時の処理
+end
+
+wallet = 100
+if wallet >= 120
+    puts "ジュースを買おう"
+else
+    puts "お金じゃ買えない幸せがたくさんあるんだ"
+end
+
+#どちらかの条件を満たすとき　if a || b (aまたはb)
+x = 200
+if x <= 0 || x >= 100
+    puts "範囲外です"
+end
+
+x = 0
+y = -1
+z = 1
+if x > 0 || y > 0 || z > 0
+    puts "正の数です"
+end
+
+#両方の条件を満たすとき　if a && b (aかつb)
+wallet = 500
+weather = "fine"
+if wallet >= 300 && weather == "fine"
+    puts "コンビニでアイスを買おう"
+end
+
+#複数の道から1つを選んで分岐する-case
+season = "秋"
+case season
+when "春"
+    puts "アイスを買っていこう！"
+when "夏"
+    puts "かき氷買っていこう！"
+else
+    puts "あんまん買っていこう！"
+end
+
+#決まった回数だけ繰り返す-timesメソッド
+2.times do
+    puts "カフェラテ"
+    2.times do
+        puts "モカ"
+    end
+end
+puts "フラペチーノ"
+
+2.times {
+    puts "カフェラテ"
+    2.times{
+        puts "モカ"
+    }
+}
+puts "フラペチーノ"
+
+#条件付き繰り返し　while
+biscuit = 0
+while biscuit < 2
+    biscuit = biscuit + 1
+    puts "ポケットをたたくとビスケットが#{biscuit}つ"
+end
